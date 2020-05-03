28b53361e115b6f097a75c52fd7286366b47b34b
xinil502 <1223176327@qq.com>
2020-05-03 21:10:46 +0800

    Project10增加对象的转型。

--- src/learnJava/Project10.java
+++ src/learnJava/Project10.java
@@ -7,16 +7,22 @@ protected  子类可以访问。
 public  所以类都可以访问。
 
 多态：继承，重写，父类引用子类
+
+转型：向上自动，向下强制。
  */
 public class Project10 {
     public static void main(String[] args) {
         Father p1 = new Father();
         Child1 p2 = new Child1();
         Child2 p3 = new Child2();
-
         funn(p1);
         funn(p2);
         funn(p3);
+
+        Father a = new Child1(); //向上自动转型。
+//      a.test1(); 新对象自动转为Father，没有test1方法。
+        Child2 b = (Child2)a;//向下强制转型。
+        b.test2();
     }
 
     static void funn(Father p){  //多态可以根据父类去引用子类。
@@ -39,9 +45,15 @@ class Child1 extends Father{
     public void fun(){
         System.out.println("执行Child1类。");
     }
+    public void test1(){
+        System.out.println("执行父类中没有的test1");
+    }
 }
 class Child2 extends Father{
     public void fun(){
         System.out.println("执行Child2类。");
+    }
+    public void test2(){
+        System.out.println("执行父类中没有的test2");
     }
 }
