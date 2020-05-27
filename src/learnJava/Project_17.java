7f7ee7d263e86d47a9b449bd02b066132b7a176f
xinil502 <1223176327@qq.com>
2020-05-28 01:57:22 +0800

    Project_17.Arrays排序方法与二分查找方法

--- src/learnJava/Project_17.java
+++ src/learnJava/Project_17.java
@@ -16,7 +16,14 @@ public class Project_17 {
         str1 = Arrays.copyOf(str1, strLen1 + strLen2);// 扩容
         System.arraycopy(str2, 0, str1, strLen1, strLen2);
         // 被拷贝的数组，起始索引值，拷贝至的数组，起始索引值，拷贝长度
-        System.out.println(Arrays.toString(str1)); //import java.util.Arrays;原格式输出数组
 
+        //java.util.Arrays 工具类
+        System.out.println(Arrays.toString(str1)); //原格式输出数组
+
+        int[] nums = {1, 2, 2, 3, 1, 7, 6, 3, 2, 7};
+        Arrays.sort(nums); //从小到大排序
+        System.out.println(Arrays.toString(nums)); //输出排序结果
+        System.out.println(Arrays.binarySearch(nums,3)); //对已排序的数组进行二分查找
+        System.out.println(Arrays.binarySearch(nums,9)); //return -(二分查找最后一次的左边界索引值-1)
     }
 }
