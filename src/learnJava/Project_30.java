a2fe139d92578e12b1a31677e0da39d7fc0929ee
xinil502 <1223176327@qq.com>
2020-06-27 02:25:51 +0800

    Project_30.Collection接口中的基本常用方法

--- src/learnJava/Project_30.java
+++ src/learnJava/Project_30.java
@@ -2,12 +2,18 @@ package learnJava;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /*
  * Collection接口中的基本常用方法
  */
 public class Project_30 {
     public static void main(String[] args) {
+        test01();
+        test02();
+    }
+
+    public static void test01() {
         Collection<String> c = new ArrayList<>();
 
 
@@ -19,6 +25,31 @@ public class Project_30 {
         Object[] obj = c.toArray();  //转为对象数组
         c.remove("第一个泛型元素"); //移除，不是delete删除（删地址，不是删对象）
         c.clear();  //移出所有的元素
+    }
+
+    public static void test02(){
+        List<String> list01 = new ArrayList<>();
+        list01.add("aa");
+        list01.add("bb");
+        list01.add("cc");
+
+        List<String> list02 = new ArrayList<>();
+        list02.add("aa");
+        list02.add("dd");
+        list02.add("ee");
+
+        System.out.println("list01:" + list01);
+
+        list01.addAll(list02);   //添加至集合中
+        System.out.println("list01:" + list01);
+
+        System.out.println(list01.containsAll(list02)); //该集合是否包含后面的集合
+
+        list01.removeAll(list02);   //删除两集合中相同的元素
+        System.out.println("list01:" + list01);
+
+        list01.retainAll(list02);//  取交集
+        System.out.println("list01:" + list01);
 
 
     }
