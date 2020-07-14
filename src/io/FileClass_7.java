907412da3fea1212f22a60025ee422ec8002813e
xinil502 <1223176327@qq.com>
2020-07-14 18:00:27 +0800

    字节数组流

--- src/io/FileClass_7.java
+++ src/io/FileClass_7.java
@@ -15,15 +15,23 @@ import java.io.*;
 public class FileClass_7 {
     public static void main(String[] args) {
         testByteArrayInputStream("This is a sentence.");
+        byte[] src = testByteArrayOutputStream("abcdefg");
     }
 
+    /**
+     * 1.从内存中获取字节数组进行操作**（计算，写入文件）
+     *
+     *
+     * 跟 FileInputStream一样，构造时用数据源做参数(文件路径 / 此处为字节数组)
+     * 使用while((len=in.read(temp))!=-1){
+     *     //使用读取到的数据。
+     * }
+     */
     public static void testByteArrayInputStream(String s){
-        byte[] src = s.getBytes();
-
-        InputStream is = null;
+        byte[] src = s.getBytes(); //输入的字节数组
 
         try {
-            is = new ByteArrayInputStream(src);
+            InputStream is = new ByteArrayInputStream(src);
             byte[] flush = new byte[10]; //缓冲容器
             int len = -1;
             while((len = is.read(flush)) != -1){
@@ -34,12 +42,16 @@ public class FileClass_7 {
         }
     }
 
-    public static void testByteArrayOutputStream(String s){
+    /**
+     * 将程序运行中获取的信息写到到内存的字节数组中，暂时储存。
+     *
+     */
+    public static byte[] testByteArrayOutputStream(String s){
         byte[] src = s.getBytes();
 
-        ByteArrayOutputStream baos = null;
-
-        baos = new ByteArrayOutputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write(src, 0, src.length);
+
+        return src;
     }
 }
