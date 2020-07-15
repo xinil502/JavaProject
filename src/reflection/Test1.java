198f6009aa15bef4ec994686dede230e78d3a864
xinil502 <1223176327@qq.com>
2020-07-15 18:16:53 +0800

    获取类的构造的名称，修饰符，参数类型

--- src/reflection/Test1.java
+++ src/reflection/Test1.java
@@ -11,12 +11,14 @@ public class Test1 {
             Class cp = c.getSuperclass();//获取父类
             System.out.println(c + "的父类：" + cp);
 
+
             Class[] interfaces = c.getInterfaces();//获取当前类的所有接口
             int i=1;
             for(Class temp: interfaces){
                 System.out.println(c + "的接口" + i + ":" + temp);
                 ++i;
             }
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
