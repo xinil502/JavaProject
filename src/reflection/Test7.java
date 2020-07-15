7477819787c17bfff3f3ba0d4a8a1e49f9d94ab4
xinil502 <1223176327@qq.com>
2020-07-15 19:35:28 +0800

    反射调用制定属性

---
+++ src/reflection/Test7.java
@@ -0,0 +1,30 @@
+package reflection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+/**
+ *
+ * 反射调用指定属性
+ *
+ * get调用本类和父类的 public 信息
+ * getDeclare调用本类的所有属性（包括私有）
+ */
+public class Test7 {
+    public static void main(String[] args) {
+        try {
+            Class c = Class.forName("reflection.Student");
+            Constructor con  = c.getConstructor();
+            Object obj = con.newInstance();
+            Student stu = (Student)obj;
+
+            stu.name = "某某人";
+            Field f = c.getField("name");
+            System.out.println(f.get(stu));
+            f.set(stu, "某某");
+            System.out.println(f.get(stu));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
