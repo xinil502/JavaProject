2aba1aef74d94cd84add422f8d5a728c19b20552
xinil502 <1223176327@qq.com>
2020-08-06 15:24:46 +0800

    PreparedStatement学习

--- src/jdbc/JdbcTest_4.java
+++ src/jdbc/JdbcTest_4.java
@@ -1,5 +1,6 @@
 package jdbc;
 
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.*;
@@ -11,7 +12,12 @@ import java.util.Properties;
  *      即使用户的信息含有关键字，也不会起作用。
  *      PreparedStatement 继承了 java,sql.Statement
  *      PreparedStatement 预先对 SQL 框架进行编译，然后只能对框架传值。
- *
+ * PreparedStatement 和 Statement 的比较：
+ *      -Statement有SQL注入问题，PreparedStatement解决了注入问题。
+ *      -Statement是编译SQL语句一次，执行一次。 PreparedStatement是编译一次，可以执行N次。
+ *      -PreparedStatement会在编译时进行类型检查。
+ *      -PreparedStatement 使用较多。
+ *          极少数使用 Statement(业务方面要求修改SQL语句结构式，必须支持SQL注入)。
  */
 public class JdbcTest_4 {
     public static void main(String[] args) {
@@ -35,15 +41,14 @@ public class JdbcTest_4 {
 
             //3.获取预编译的数据库操作对象，，，SQL语句
             /**
-             *  单独一个 ? 作为占位符  , 一个 ? 接收一个值.
-             *  传给DBMS进行预编译。
+             *使用PreparedStatement方式：
+             *     1.单独一个 ? 作为占位符  , 一个 ? 接收一个值.传给DBMS进行预编译。
              *
-             *
-             *  ps.set数据类型(parameterIndex, x)传对应数据类型的值：
-             *          parameterIndex：第一个问号的下标是1，第二个问号的下标是2。
-             *          x: 传入的值。
-             *          如果数据类型是String，会在前后自动加'，不需要在值里面加了
-             *
+             *     2.ps.set数据类型(parameterIndex, x)传对应数据类型的值：
+             *             parameterIndex：第一个问号的下标是1，第二个问号的下标是2。
+             *             x: 传入的值。
+             *             如果数据类型是String，会在前后自动加'，不需要在值里面加了
+             *     3.ps.executeUpdate()/ps.executeQuery() 执行SQL语句。
              */
             ps = conn.prepareStatement("SELECT empno a,ename,sal FROM emp WHERE ename = ?"); //SQL框架
             ps.setString(1,"SMITH");
