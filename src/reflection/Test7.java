868f030ddd2d40df4e1c4f2cc2e92acb0d31d142
xinil502 <1223176327@qq.com>
2020-09-16 12:45:44 +0800

    反射调用指定属性

--- src/reflection/Test7.java
+++ src/reflection/Test7.java
@@ -13,16 +13,14 @@ import java.lang.reflect.Field;
 public class Test7 {
     public static void main(String[] args) {
         try {
-            Class c = Class.forName("reflection.Student");
-            Constructor con  = c.getConstructor();
-            Object obj = con.newInstance();
-            Student stu = (Student)obj;
+            Class c = Class.forName("reflection.Student");//反射获取类
+            Constructor con  = c.getConstructor();//获取构造器
+            Object obj = con.newInstance();//构造对象
+            Student stu = (Student)obj;//转为对应对象
 
-            stu.name = "某某人";
-            Field f = c.getField("name");
-            System.out.println(f.get(stu));
-            f.set(stu, "某某");
-            System.out.println(f.get(stu));
+            Field f = c.getField("name"); //确定要操作的属性f
+            f.set(stu, "某某");//修改属性值
+            System.out.println(f.get(stu));//获取属性值
         } catch (Exception e) {
             e.printStackTrace();
         }
